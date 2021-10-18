a87d3a844a84c78e9e6fe74cba9f65c0817a1558
Eduard Kopylov <ed_010@mail.ru>
2021-10-19 00:51:56 +0300

    fix the gitignore

--- my-recipe-book-app/src/environments/environment.prod.ts
+++ my-recipe-book-app/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  firebaseAPIKEY : 'AIzaSyD-9U6lxoFUeLO3utVkxe2sqdL1tLfBlnQ'
+  firebaseAPIKEY : 'AIzaSyD-9U6lxoFUeLO3utVkxe2sqdL1tLfBlnQ',
+  api:'1'
 };
